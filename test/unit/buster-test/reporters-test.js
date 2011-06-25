1f44e0cee7f755ee99f907c887b8cc0c0d4e1203
Christian Johansen <christian@gitorious.org>
2011-06-26 00:22:36 +0200

    Add a loader for reporters

---
+++ test/unit/buster-test/reporters-test.js
@@ -0,0 +1,19 @@
+var buster = { reporters: require("../../../lib/buster-test/reporters") };
+buster.util = require("buster-util");
+buster.moduleLoader = require("buster-module-loader");
+var assert = require("buster-assert");
+var sinon = require("sinon");
+
+buster.util.testCase("Reporters test", sinon.testCase({
+    "should load built-in reporter": function () {
+        assert.equals(buster.reporters.xml, buster.reporters.load("xml"));
+    },
+
+    "should get reporter through the moduleLoader": function () {
+        this.stub(buster.moduleLoader, "load");
+        buster.reporters.load("my-custom#reporter");
+
+        assert(buster.moduleLoader.load.calledOnce);
+        assert(buster.moduleLoader.load.calledWith("my-custom#reporter"));
+    }
+}, "should"));
