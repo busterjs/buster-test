26c7667cd4404a721ba0207fc013ca5f2155e34b
Christian Johansen <christian@cjohansen.no>
2012-09-29 13:42:56 +0200

    Update teamcity reporter

--- run-tests.js
+++ run-tests.js
@@ -13,7 +13,7 @@ require("./test/unit/buster-test/reporters/html-test");
 require("./test/unit/buster-test/reporters/json-proxy-test");
 require("./test/unit/buster-test/reporters/specification-test");
 require("./test/unit/buster-test/reporters/tap-test");
-// require("./test/unit/buster-test/reporters/teamcity-test");
+require("./test/unit/buster-test/reporters/teamcity-test");
 // require("./test/unit/buster-test/reporters/xml-test");
 require("./test/unit/buster-test/spec-test");
 require("./test/unit/buster-test/test-case-test");
--- test/unit/buster-test/reporters/teamcity-test.js
+++ test/unit/buster-test/reporters/teamcity-test.js
@@ -1,100 +1,93 @@
+var helper = require("../../../test-helper");
+var rhelper = require("./test-helper");
+var bane = require("bane");
+var assert = require("referee").assert;
 var sinon = require("sinon");
-var buster = require("buster-core");
-var teamcityReporter = require("../../../../lib/buster-test/reporters/teamcity");
-var busterUtil = require("buster-util");
-var util = require("util");
-var assert = require("buster-assertions").assert;
+var teamCity = require("../../../../lib/buster-test/reporters/teamcity");
 
-busterUtil.testCase("TeamcityReporterTest", sinon.testCase({
-    setUp:function () {
-        var that = this;
-        that.reporter = teamcityReporter.create();
-        that.log = [];
-
-        sinon.stub(util, "puts", function (msg) {
-            that.log.push(msg);
-        });
+helper.testCase("TeamCityReporterTest", {
+    setUp: function () {
+        this.outputStream = rhelper.writableStream();
+        this.assertIO = rhelper.assertIO;
+        this.runner = bane.createEventEmitter();
+        this.runner.console = bane.createEventEmitter();
+        this.reporter = teamCity.create({
+            outputStream: this.outputStream
+        }).listen(this.runner);
+        this.clock = sinon.useFakeTimers();
     },
 
-    tearDown:function () {
-        util.puts.restore();
+    tearDown: function () {
+        this.clock.restore();
     },
 
-    "should print testsuite element with stats on context:end":function () {
-        this.reporter.contextStart({ name:"Context" });
-        this.reporter.contextEnd({ name:"Context" });
+    "prints testsuite element with stats on context:end":function () {
+        this.runner.emit("context:start", { name:"Context" });
+        this.runner.emit("context:end", { name:"Context" });
 
-        assert.equals(this.log, [
-            "##teamcity[testSuiteStarted name='Context']",
-            "##teamcity[testSuiteFinished name='Context']"
-        ]);
+        this.assertIO("##teamcity[testSuiteStarted name='Context']");
+        this.assertIO("##teamcity[testSuiteFinished name='Context']");
     },
 
-    "should not print testsuite element for nested context:end":function () {
-        this.reporter.contextStart({ name:"Context" });
-        this.reporter.contextStart({ name:"Inner" });
-        this.reporter.contextEnd({ name:"Inner" });
+    "does not print testsuite element for nested context:end":function () {
+        this.runner.emit("context:start", { name:"Context" });
+        this.runner.emit("context:start", { name:"Inner" });
+        this.runner.emit("context:end", { name:"Inner" });
 
-        assert.equals(this.log, []);
+        assert.equals(this.outputStream.toString(), "");
     },
 
-    "should print total time for each test":function () {
-        this.reporter.contextStart({ name:"Context" });
-        this.reporter.testStart({ name:"should #1" });
+    "prints total time for each test":function () {
+        this.runner.emit("context:start", { name:"Context" });
+        this.runner.emit("test:start", { name:"should #1" });
         this.clock.tick(10);
-        this.reporter.testSuccess({ name:"should #1" });
-        this.reporter.testStart({ name:"should #2" });
+        this.runner.emit("test:success", { name:"should #1" });
+        this.runner.emit("test:start", { name:"should #2" });
         this.clock.tick(20);
-        this.reporter.testSuccess({ name:"should #2" });
-        this.reporter.contextEnd({ name:"Context" });
+        this.runner.emit("test:success", { name:"should #2" });
+        this.runner.emit("context:end", { name:"Context" });
 
-        assert.equals(this.log, [
-            "##teamcity[testSuiteStarted name='Context']",
-            "##teamcity[testStarted name='should #1' captureStandardOutput='true']",
-            "##teamcity[testFinished name='should #1' duration='10']",
-            "##teamcity[testStarted name='should #2' captureStandardOutput='true']",
-            "##teamcity[testFinished name='should #2' duration='20']",
-            "##teamcity[testSuiteFinished name='Context']"
-        ]);
+        this.assertIO("##teamcity[testSuiteStarted name='Context']");
+        this.assertIO("##teamcity[testStarted name='should #1' captureStandardOutput='true']");
+        this.assertIO("##teamcity[testFinished name='should #1' duration='10']");
+        this.assertIO("##teamcity[testStarted name='should #2' captureStandardOutput='true']");
+        this.assertIO("##teamcity[testFinished name='should #2' duration='20']");
+        this.assertIO("##teamcity[testSuiteFinished name='Context']");
     },
 
-    "should add nested context names to test names":function () {
-        this.reporter.contextStart({ name:"Context" });
-        this.reporter.contextStart({ name:"Some behavior" });
-        this.reporter.testStart({ name:"should #1" });
-        this.reporter.testSuccess({ name:"should #1" });
-        this.reporter.testStart({ name:"should #2" });
-        this.reporter.testSuccess({ name:"should #2" });
-        this.reporter.contextEnd({ name:"Some behavior" });
-        this.reporter.contextEnd({ name:"Context" });
+    "adds nested context names to test names":function () {
+        this.runner.emit("context:start", { name:"Context" });
+        this.runner.emit("context:start", { name:"Some behavior" });
+        this.runner.emit("test:start", { name:"should #1" });
+        this.runner.emit("test:success", { name:"should #1" });
+        this.runner.emit("test:start", { name:"should #2" });
+        this.runner.emit("test:success", { name:"should #2" });
+        this.runner.emit("context:end", { name:"Some behavior" });
+        this.runner.emit("context:end", { name:"Context" });
 
-        assert.equals(this.log, [
-            "##teamcity[testSuiteStarted name='Context']",
-            "##teamcity[testStarted name='Some behavior should #1' captureStandardOutput='true']",
-            "##teamcity[testFinished name='Some behavior should #1' duration='0']",
-            "##teamcity[testStarted name='Some behavior should #2' captureStandardOutput='true']",
-            "##teamcity[testFinished name='Some behavior should #2' duration='0']",
-            "##teamcity[testSuiteFinished name='Context']"
-        ]);
+        this.assertIO("##teamcity[testSuiteStarted name='Context']");
+        this.assertIO("##teamcity[testStarted name='Some behavior should #1' captureStandardOutput='true']");
+        this.assertIO("##teamcity[testFinished name='Some behavior should #1' duration='0']");
+        this.assertIO("##teamcity[testStarted name='Some behavior should #2' captureStandardOutput='true']");
+        this.assertIO("##teamcity[testFinished name='Some behavior should #2' duration='0']");
+        this.assertIO("##teamcity[testSuiteFinished name='Context']");
     },
 
-    "should include elements for errors/failures":function () {
-        this.reporter.contextStart({ name:"Context" });
-        this.reporter.testStart({ name:"#1" });
-        this.reporter.testFailure({ name:"#1", error:{ name:'E1', message:'M1' } });
-        this.reporter.testStart({ name:"#2" });
-        this.reporter.testError({ name:"#2", error:{ name:'E2', message:'M2' } });
-        this.reporter.contextEnd({ name:"Context" });
+    "includes elements for errors/failures":function () {
+        this.runner.emit("context:start", { name:"Context" });
+        this.runner.emit("test:start", { name:"#1" });
+        this.runner.emit("test:failure", { name:"#1", error:{ name:'E1', message:'M1' } });
+        this.runner.emit("test:start", { name:"#2" });
+        this.runner.emit("test:error", { name:"#2", error:{ name:'E2', message:'M2' } });
+        this.runner.emit("context:end", { name:"Context" });
 
-        assert.equals(this.log, [
-            "##teamcity[testSuiteStarted name='Context']",
-            "##teamcity[testStarted name='#1' captureStandardOutput='true']",
-            "##teamcity[testFailed name='#1' message='Error-> E1 - M1|n' details='Error-> E1 - M1|n']",
-            "##teamcity[testFinished name='#1' duration='0']",
-            "##teamcity[testStarted name='#2' captureStandardOutput='true']",
-            "##teamcity[testFailed name='#2' message='Error-> E2 - M2|n' details='Error-> E2 - M2|n']",
-            "##teamcity[testFinished name='#2' duration='0']",
-            "##teamcity[testSuiteFinished name='Context']"
-        ]);
+        this.assertIO("##teamcity[testSuiteStarted name='Context']");
+        this.assertIO("##teamcity[testStarted name='#1' captureStandardOutput='true']");
+        this.assertIO("##teamcity[testFailed name='#1' message='Error-> E1 - M1|n' details='Error-> E1 - M1|n']");
+        this.assertIO("##teamcity[testFinished name='#1' duration='0']");
+        this.assertIO("##teamcity[testStarted name='#2' captureStandardOutput='true']");
+        this.assertIO("##teamcity[testFailed name='#2' message='Error-> E2 - M2|n' details='Error-> E2 - M2|n']");
+        this.assertIO("##teamcity[testFinished name='#2' duration='0']");
+        this.assertIO("##teamcity[testSuiteFinished name='Context']");
     }
-}, "should"));
+});
